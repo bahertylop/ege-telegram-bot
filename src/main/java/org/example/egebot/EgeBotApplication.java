f767d5f20e207681d81dd1159a24389e0b9f702d
bahertylop <levichankin@gmail.com>
2024-03-26 20:34:51 +0300

    git init commit

---
+++ src/main/java/org/example/egebot/EgeBotApplication.java
@@ -0,0 +1,13 @@
+package org.example.egebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgeBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgeBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/egebot/EgeBotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.egebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgeBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
