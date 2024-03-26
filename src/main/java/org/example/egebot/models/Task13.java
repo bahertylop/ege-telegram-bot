8a44eb8da9e387128e8ef19f7969ae2c7dc968ce
bahertylop <levichankin@gmail.com>
2024-03-26 22:59:41 +0300

    models for tasks commit

---
+++ src/main/java/org/example/egebot/models/Task13.java
@@ -0,0 +1,33 @@
+package org.example.egebot.models;
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Task13 {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "task_id")
+    private Long id;
+
+    @Column(length = 2000)
+    private String task;
+
+    private String answer;
+
+    @Column(length = 1500)
+    @Nullable
+    private String clarification;
+
+    @Column(name = "task_number")
+    private Integer taskNumber;
+}
